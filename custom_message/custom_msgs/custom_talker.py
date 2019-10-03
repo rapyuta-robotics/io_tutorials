2751d805aa898a75b57d267a199b85a73e37f3f2
Amit  Singh <amitsingh21991@gmail.com>
2019-10-03 15:11:59 +0530

    added new line to generate new pull request

--- custom_message/custom_msgs/custom_talker.py
+++ custom_message/custom_msgs/custom_talker.py
@@ -5,6 +5,7 @@ import rospy
 from custom_msgs.msg import ChargeMetric, ChargeStatus
 
 def custom_talker():
+
 	topic_name = os.getenv('topic_name', 'custom_message')
 	pub = rospy.Publisher(topic_name, ChargeMetric, queue_size=10)
 	rospy.init_node('custom_message_talker', anonymous=True)
