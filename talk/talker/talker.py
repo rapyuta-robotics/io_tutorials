f6f1e353a83c725f9e595fc3909aa04816001f5b
Prince Jha <prince.jha@rapyuta-robotics.com>
2020-05-15 09:41:15 +0530

    Modified talker to print env vars

--- talk/talker/talker.py
+++ talk/talker/talker.py
@@ -2,13 +2,14 @@
 
 import rospy
 from std_msgs.msg import String
+import os
 
 def talker():
 	pub = rospy.Publisher('telemetry', String, queue_size=10)
 	rospy.init_node('talker', anonymous=True)
 	rate = rospy.Rate(10)
 	while not rospy.is_shutdown():
-		hello_str = "hello_world %s" % rospy.get_time()
+		hello_str = "hello_world {} {} {} {}".format(os.getenv('RIO_DEVICE_UUID'), os.getenv('RIO_DEVICE_NAME_LAST_SEEN'), os.getenv('RIO_PROJECT_UUID'), os.getenv('RIO_ROS_ENV_ALIAS'))
 		rospy.loginfo(hello_str)
 		pub.publish(hello_str)
 		rate.sleep()
