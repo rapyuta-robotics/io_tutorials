b8fec49ea9ddc3df8d957c30dfdc3a7553fe9d47
Shiv Mohith <shiv.mohith@rapyuta-robotics.com>
2022-10-03 11:17:43 +0530

    talker: accepts variable rate of publishing

--- talk/talker/talker.py
+++ talk/talker/talker.py
@@ -2,19 +2,24 @@
 
 import rospy
 from std_msgs.msg import String
+import os
+
+RATE = float(os.getenv("RATE", 10))
+
 
 def talker():
-	pub = rospy.Publisher('telemetry', String, queue_size=10)
-	rospy.init_node('talker', anonymous=True)
-	rate = rospy.Rate(10)
-	while not rospy.is_shutdown():
-		hello_str = "hello_world %s" % rospy.get_time()
-		rospy.loginfo(hello_str)
-		pub.publish(hello_str)
-		rate.sleep()
+    pub = rospy.Publisher('telemetry', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(RATE)
+    while not rospy.is_shutdown():
+        hello_str = "hello_world %s" % rospy.get_time()
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+        rate.sleep()
+
 
 if __name__ == '__main__':
-	try:
-		talker()
-	except rospy.ROSInterruptException:
-		pass
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
