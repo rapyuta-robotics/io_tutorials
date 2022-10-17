26955e0fb7556682b97db93ab0e4d845c0c975a2
Swagnik Dutta <swagnikd@gmail.com>
2022-10-17 19:39:45 +0530

    using a single python file usign rosparams

---
+++ talk/throttle_latch/throttling/talker.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import os
+import rospy
+from std_msgs.msg import String
+
+
+def talker():
+    rospy.init_node('placeholder', anonymous=True)
+    topic_name = rospy.get_param('~topic_name')
+    topic_rate = rospy.get_param('~topic_rate')
+
+    pub = rospy.Publisher(topic_name, String, queue_size=10)
+    rate = rospy.Rate(topic_rate)
+
+    while not rospy.is_shutdown():
+        msg = "talker is publishing at rate: {}".format(topic_rate)
+        rospy.loginfo(msg)
+        pub.publish(msg)
+        rate.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
