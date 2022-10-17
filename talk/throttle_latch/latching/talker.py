cfb7c86971cfd1982f6ac905ee6858db41da5146
Swagnik Dutta <swagnikd@gmail.com>
2022-10-17 20:59:36 +0530

    did same exercise for latching package

--- talk/throttle_latch/latching/talker.py
+++ talk/throttle_latch/latching/talker.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy
 from std_msgs.msg import String
@@ -9,16 +9,24 @@ Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 """
+NEEDLE = "Uploading map"
+
 
 def talker():
-	pub = rospy.Publisher('/coordinates', String, queue_size=10)
-	rospy.init_node('fast_talker', anonymous=True)
-	rate = rospy.Rate(1000)
+	rospy.init_node('placeholder', anonymous=True)
+	topic_name = rospy.get_param('~topic_name')
+	topic_rate = rospy.get_param('~topic_rate')
+	latched = True if str(rospy.get_param('~latched')) in ['true', 'True'] else False
+
+	pub = rospy.Publisher(topic_name, String, queue_size=10, latch=latched)
+	rate = rospy.Rate(topic_rate)
+
 	while not rospy.is_shutdown():
-		msg = HAYSTACK
-		rospy.loginfo('Fast talker is publishing')
+		msg = HAYSTACK if topic_name == "/coordinates" else NEEDLE
+		rospy.loginfo(msg)
 		pub.publish(msg)
 		rate.sleep()
+
 
 if __name__ == '__main__':
 	try:
