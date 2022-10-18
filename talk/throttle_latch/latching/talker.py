3f2577c698d119984bf1c0b213137fa4dd91ae38
Swagnik Dutta <swagnikd@gmail.com>
2022-10-18 15:23:07 +0530

    Throttling latching testing (#39)
    * added latch testing folder
    
    * added 4 publishers to publish on 4 different channels
    
    * made RATE variable configurable
    
    * refactor: changed structure of throttle latch packages
    
    * changed to python2
    
    * using a single python file usign rosparams
    
    * did same exercise for latching package
    
    * minor change
    
    * undo

---
+++ talk/throttle_latch/latching/talker.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import String
+
+HAYSTACK = """
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+"""
+NEEDLE = "Uploading map"
+
+
+def talker():
+	rospy.init_node('node', anonymous=True)
+	topic_name = rospy.get_param('~topic_name')
+	topic_rate = rospy.get_param('~topic_rate')
+	latched = True if str(rospy.get_param('~latched')) in ['true', 'True'] else False
+
+	pub = rospy.Publisher(topic_name, String, queue_size=10, latch=latched)
+	rate = rospy.Rate(topic_rate)
+
+	while not rospy.is_shutdown():
+		msg = HAYSTACK if topic_name == "/coordinates" else NEEDLE
+		rospy.loginfo(msg)
+		pub.publish(msg)
+		rate.sleep()
+
+
+if __name__ == '__main__':
+	try:
+		talker()
+	except rospy.ROSInterruptException:
+		pass
